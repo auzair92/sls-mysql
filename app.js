41ed8560f61b4bd52ffa42ef79263354e7c1cbbc
Uzair Ahmed <17614117+auzair92@users.noreply.github.com>
2024-08-25 14:12:20 +0500

    8th

--- app.js
+++ app.js
@@ -55,6 +55,6 @@ app.use((err, req, res, next) => {
 
 // Start the server
 const PORT = 8080;
-app.listen(PORT, "0.0.0.0" () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port: ${PORT}`);
 });
